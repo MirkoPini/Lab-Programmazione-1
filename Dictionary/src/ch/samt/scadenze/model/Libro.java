52cb358643d00eef8e827e98cdd49f00e28d8a81
MirkoPini <hic866@student.edu.ti.ch>
2025-12-21 11:57:02 +0100

    Pini

--- Dictionary/src/ch/samt/scadenze/model/Libro.java
+++ Dictionary/src/ch/samt/scadenze/model/Libro.java
@@ -3,5 +3,22 @@ package ch.samt.scadenze.model;
 public class Libro extends Element{
     private int durataGiorni;
 
-    public
+    public Libro(String idCode, Date creationDate, int durataGiorni){
+        super(idCode, creationDate);
+        this.durataGiorni = durataGiorni;
+    }
+
+    public int getDurataGiorni(){
+        return this.durataGiorni;
+    }
+
+    public Date dataScadenza(){
+        Date datascadenza = creationDate + durataGiorni;
+        return datascadenza;
+    }
+
+    @Override
+    public String toString(){
+        return super(toString) + "Prestito:" + "\n" + "Durata giorni: " + getDurataGiorni() + "\n";
+    }
 }
