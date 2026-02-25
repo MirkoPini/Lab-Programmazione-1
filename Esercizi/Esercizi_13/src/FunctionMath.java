d90678613490c13c0be911314556926bece1d9ba
Mirko Pini <hic866@student.edu.ti.ch>
2026-02-25 15:14:41 +0100

    Pini

---
+++ Esercizi/Esercizi_13/src/FunctionMath.java
@@ -0,0 +1,24 @@
+public class FunctionMath {
+    public static int abs(int x){
+        return Math.abs(x);
+    }
+
+    public static double abs(double x){
+        return Math.abs(x);
+    }
+
+    public static boolean isPrime(int n){
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static double hypotenuse(double a, double b){
+        return Math.hypot(a, b);
+    }
+}
---
+++ Esercizi/Esercizi_13/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FunctionMath.abs(-3323));
+        System.out.println(FunctionMath.abs(-23.2));
+        System.out.println(FunctionMath.isPrime(5132));
+        System.out.println(FunctionMath.hypotenuse(23.2, 13.3));
+    }
+}
