d6a227bd91ce96f2237bfbe9e5c3cfbc3ecb77c3
Mirko Pini <hic866@student.edu.ti.ch>
2026-02-27 13:25:53 +0100

    Pini

--- Esercizi/Esercizi_13/src/FunctionMath.java
+++ Esercizi/Esercizi_13/src/FunctionMath.java
@@ -8,12 +8,15 @@ public class FunctionMath {
     }
 
     public static boolean isPrime(int n){
-        if (n <= 1)
+        if (n <= 1) {
             return false;
+        }
 
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
                 return false;
+            }
+        }
 
         return true;
     }
--- Esercizi/Esercizi_13/src/Main.java
+++ Esercizi/Esercizi_13/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(FunctionMath.abs(-3323));
         System.out.println(FunctionMath.abs(-23.2));
-        System.out.println(FunctionMath.isPrime(5132));
+        System.out.println(FunctionMath.isPrime(2));
         System.out.println(FunctionMath.hypotenuse(23.2, 13.3));
     }
 }
